d59f7cf71357f18ff2fcea259080d00639151422
ricpato07 <ricpato07@gmail.com>
2019-07-05 14:00:47 -0500

    Create Constants.ts

---
+++ src/app/globals/Constants.ts
@@ -0,0 +1,15 @@
+
+// export const NOMBRE_PATTERN = /^[a-zA-Z\. ñÑáéíóúÁÉÍÓÚ]*$/;
+// export const INTEGER_PATTERN = /^[0-9]*$/;
+// export const CP_PATTERN = /^[0-9]{5}$/;
+// export const NUM_CASA_PATTERN = /^[0-9\#]*$/;
+
+export class Constants {
+    public static readonly NOMBRE_PATTERN = /^[a-zA-Z\. ñÑáéíóúÁÉÍÓÚ]*$/;
+    public static readonly INTEGER_PATTERN =  /^[0-9]*$/;
+    public static readonly CP_PATTERN =  /^[0-9]{5}$/;
+    public static readonly NUM_CASA_PATTERN =  /^[0-9\#]*$/;
+    public static readonly MONEDA_PATTERN = /^[0-9]{1,7}(\.[0-9]{1,2})?$/;
+    public static readonly IMG_PATH = "./../../../assets/img/";
+
+}
