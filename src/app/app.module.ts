1e773cc351646c765565d9dd1417f883ff216c0c
ricpato07 <ricpato07@gmail.com>
2019-07-05 13:50:42 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {routing, appRoutingProviders} from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { HttpClientModule } from '@angular/common/http';
 
+// proyecto principal
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmpleadoComponent } from './empleado/empleado.component';
-import { FrutaComponent } from './fruta/fruta.component';
-import { HomeComponent } from './home/home.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { ConversorPipe } from './pipes/conversor.pipe';
-import { CochesComponent } from './coches/coches.component';
-import {HttpClientModule} from '@angular/common/http';
-import {PlantillasComponent } from './plantillas/plantillas.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+//modules
+import { AtomComponentsModule } from './modules/atom-components.module';
+
+//principal components
+import { BasicosContratanteComponent } from './components/basicos-contratante/basicos-contratante.component';
+import { DatosInmuebleComponent } from './components/datos-inmueble/datos-inmueble.component';
+import { CotizacionComponent } from './components/cotizacion/cotizacion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoComponent,
-    FrutaComponent,
-    HomeComponent,
-    ContactoComponent,
-    ConversorPipe,
-    CochesComponent,
-    PlantillasComponent
+    BasicosContratanteComponent,
+    DatosInmuebleComponent,
+    CotizacionComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
-    routing,
-    HttpClientModule
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    FilterPipeModule,
+    HttpClientModule,
+    AtomComponentsModule
   ],
-  providers: [appRoutingProviders],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
